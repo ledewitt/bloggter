14b93dbcee26dfdcab582aa1c7c5b8e117520f85
Luke DeWitt <luke@dewittsoft.com>
2013-02-18 11:27:38 -0700

    Adding in the main bloggter.rb executable to fire up Sinatra and access the database.

---
+++ bloggter.rb
@@ -0,0 +1,34 @@
+require          'bundler/setup'
+require          'sinatra'
+require_relative 'config/database'
+require_relative 'lib/bloggter'
+require          'json'
+
+# for handle show ten most recent mweets
+
+get("/:handle") {
+  user = Bloggter::User.find_by_handle!(params[:handle])
+  mweets = user.entries.limit(10).order("created_at DESC")
+  if request.accept.include? "application/json"
+    content_type :json
+    mweets.to_json(except:  [:id, :updated_at, :user_id],
+                   include: {user: {except: [:id, :updated_at]}})
+  else
+    erb :mweets, locals: {   user:   user,
+                           mweets: mweets }
+  end
+}
+
+get('/') {
+  raise ActiveRecord::RecordNotFound
+}
+
+post('/:handle/mweeter') {
+  user = Bloggter::User.find_by_handle!(params[:handle])
+  user.mweets.create!(body: params[:mweet])
+  redirect "/#{user.handle}"
+}
+
+error(ActiveRecord::RecordNotFound) {
+  "User not found"
+}
