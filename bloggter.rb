58f1ddc43d7b04e4c7b1b6fc31ae5fb733044c6b
Luke DeWitt <luke@dewittsoft.com>
2013-02-18 16:49:47 -0700

    Adding more functionality to the home page.

--- bloggter.rb
+++ bloggter.rb
@@ -11,7 +11,15 @@ get("/") {
 }
 
 post("/") {
+  user = params[:user]
   
+  redirect "/#{user}"
+  
+  # if Bloggter::User.exists?(user)
+  #   redirect "/#{user}"
+  # else
+  #   redirect error
+  # end
 }
 
 get("/:handle") {
